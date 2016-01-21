3f32425189121587d9fd077820c20d03126f6b40
Evandro Zanatta <hemanstifler@gmail.com>
2016-01-21 02:38:59 +0000

    Salavando desenvolvimento

--- js/main.js
+++ js/main.js
@@ -3,3 +3,41 @@ var profileName = document.getElementById('profile-name');
 var profileLogin = document.getElementById('profile-login');
 profileName.innerHTML = faker.name.findName();
 profileLogin.innerHTML = faker.internet.userName();
+
+var latitude;
+
+function definePosition(position){
+    
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude
+    
+    /* global GMaps */
+    var map = new GMaps({
+        div: '#map',
+        lat: latitude,
+        lng: longitude
+    });
+    
+    map.addMarker({
+        lat: latitude,
+        lng: longitude,
+        title: 'Você',
+        click: function(e) {
+            /*alert('You clicked in this marker');*/
+        }
+    });
+    /*
+    var panorama = GMaps.createPanorama({
+      el: '#map',
+      lat : latitude,
+      lng : longitude
+    });*/
+    
+    console.log(map);
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(definePosition);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+} 
